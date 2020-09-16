ecda51f9e5463b05640095c87d862b5be8ccc6a2
deploy <932uhr923hr982haoraoiph@jfoieipwaefaoiwjoef.afwoefjowjfpawfepoa>
2020-09-16 11:05:39 +0200

    added dice images

--- 07_Yahtzeespel/Form1.Designer.cs
+++ 07_Yahtzeespel/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Btn_RollDie = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Btn_RollDie
+            // 
+            this.Btn_RollDie.Location = new System.Drawing.Point(528, 235);
+            this.Btn_RollDie.Name = "Btn_RollDie";
+            this.Btn_RollDie.Size = new System.Drawing.Size(64, 25);
+            this.Btn_RollDie.TabIndex = 0;
+            this.Btn_RollDie.Text = "Roll Dice";
+            this.Btn_RollDie.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(528, 165);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Btn_RollDie);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Btn_RollDie;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
