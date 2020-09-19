8408268cdfbf8477268b9aceccc255dcf4d4b7ab
deploy <932uhr923hr982haoraoiph@jfoieipwaefaoiwjoef.afwoefjowjfpawfepoa>
2020-09-19 17:28:52 +0200

    first commit

--- 07_Yahtzeespel/Form1.Designer.cs
+++ 07_Yahtzeespel/Form1.Designer.cs
@@ -29,37 +29,93 @@
         private void InitializeComponent()
         {
             this.Btn_RollDie = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.Die1 = new System.Windows.Forms.PictureBox();
+            this.Die2 = new System.Windows.Forms.PictureBox();
+            this.Die3 = new System.Windows.Forms.PictureBox();
+            this.Die4 = new System.Windows.Forms.PictureBox();
+            this.Die5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Die1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Die2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Die3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Die4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Die5)).BeginInit();
             this.SuspendLayout();
             // 
             // Btn_RollDie
             // 
-            this.Btn_RollDie.Location = new System.Drawing.Point(528, 235);
+            this.Btn_RollDie.Location = new System.Drawing.Point(9, 300);
             this.Btn_RollDie.Name = "Btn_RollDie";
             this.Btn_RollDie.Size = new System.Drawing.Size(64, 25);
             this.Btn_RollDie.TabIndex = 0;
             this.Btn_RollDie.Text = "Roll Dice";
             this.Btn_RollDie.UseVisualStyleBackColor = true;
+            this.Btn_RollDie.Click += new System.EventHandler(this.Btn_RollDie_Click);
             // 
-            // pictureBox1
+            // Die1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(528, 165);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.Die1.ErrorImage = null;
+            this.Die1.InitialImage = null;
+            this.Die1.Location = new System.Drawing.Point(9, 230);
+            this.Die1.Name = "Die1";
+            this.Die1.Size = new System.Drawing.Size(64, 64);
+            this.Die1.TabIndex = 1;
+            this.Die1.TabStop = false;
+            this.Die1.Click += new System.EventHandler(this.Btn_Die_Click);
+            // 
+            // Die2
+            // 
+            this.Die2.Location = new System.Drawing.Point(79, 230);
+            this.Die2.Name = "Die2";
+            this.Die2.Size = new System.Drawing.Size(64, 64);
+            this.Die2.TabIndex = 1;
+            this.Die2.TabStop = false;
+            this.Die2.Click += new System.EventHandler(this.Btn_Die_Click);
+            // 
+            // Die3
+            // 
+            this.Die3.Location = new System.Drawing.Point(149, 230);
+            this.Die3.Name = "Die3";
+            this.Die3.Size = new System.Drawing.Size(64, 64);
+            this.Die3.TabIndex = 1;
+            this.Die3.TabStop = false;
+            this.Die3.Click += new System.EventHandler(this.Btn_Die_Click);
+            // 
+            // Die4
+            // 
+            this.Die4.Location = new System.Drawing.Point(219, 230);
+            this.Die4.Name = "Die4";
+            this.Die4.Size = new System.Drawing.Size(64, 64);
+            this.Die4.TabIndex = 1;
+            this.Die4.TabStop = false;
+            this.Die4.Click += new System.EventHandler(this.Btn_Die_Click);
+            // 
+            // Die5
+            // 
+            this.Die5.Location = new System.Drawing.Point(289, 230);
+            this.Die5.Name = "Die5";
+            this.Die5.Size = new System.Drawing.Size(64, 64);
+            this.Die5.TabIndex = 1;
+            this.Die5.TabStop = false;
+            this.Die5.Click += new System.EventHandler(this.Btn_Die_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(380, 337);
+            this.Controls.Add(this.Die5);
+            this.Controls.Add(this.Die4);
+            this.Controls.Add(this.Die3);
+            this.Controls.Add(this.Die2);
+            this.Controls.Add(this.Die1);
             this.Controls.Add(this.Btn_RollDie);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Die1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Die2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Die3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Die4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Die5)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -67,7 +123,11 @@
         #endregion
 
         private System.Windows.Forms.Button Btn_RollDie;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Die1;
+        private System.Windows.Forms.PictureBox Die2;
+        private System.Windows.Forms.PictureBox Die3;
+        private System.Windows.Forms.PictureBox Die4;
+        private System.Windows.Forms.PictureBox Die5;
     }
 }
 
--- 07_Yahtzeespel/Form1.cs
+++ 07_Yahtzeespel/Form1.cs
@@ -12,10 +12,78 @@ namespace _07_Yahtzeespel
 {
     public partial class Form1 : Form
     {
+        public Bitmap[] ImgArray = new Bitmap[6];
+        public int[] Selected_Dice = { 1, 1, 1, 1, 1 };
+        public int[] Dice_Values = { 0, 0, 0, 0, 0 };
+
         public Form1()
         {
             InitializeComponent();
+            Load_Images();
+            Load_Dice();
         }
 
+        private void Load_Images()
+        {
+            for (int i = 1; i <= 6; i++)
+            {
+                ImgArray[i - 1] = new Bitmap(@$".\img\{i}.png");
+            }
+        }
+
+        private void Load_Dice()
+        {
+            PictureBox[] all_dice = { Die1, Die2, Die3, Die4, Die5 };
+
+            for (int i = 0; i < 5; i++)
+            {
+                all_dice[i].Image = ImgArray[i];
+                Dice_Values[i] = i + 1;
+            }
+        }
+
+        private void Select_Die(int element) // 1 - 5
+        {
+            if (Selected_Dice[element] == 0) {
+                Selected_Dice[element] = 1;
+            } else {
+                Selected_Dice[element] = 0;
+            }
+        }
+
+        private void Roll_Dice(int[] dice)
+        {
+            PictureBox[] all_dice = { Die1, Die2, Die3, Die4, Die5 };
+            Random rnd = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (dice[i] == 1)
+                {
+                    int value = rnd.Next(0, 6);
+                    all_dice[i].Image = ImgArray[value];
+                    Dice_Values[i] = value + 1;
+                }
+
+                Console.WriteLine(Selected_Dice[i]);
+            }
+        }
+
+        private void Btn_RollDie_Click(object sender, EventArgs e)
+        {
+            Roll_Dice(Selected_Dice);
+        }
+
+        private void Btn_Die_Click(object sender, EventArgs e)
+        {
+            PictureBox tempbox = (PictureBox)sender;
+            
+            int selected = tempbox.Name.ToCharArray().Last() - 49;
+
+            Console.WriteLine(Dice_Values[selected]);
+            Console.WriteLine(Selected_Dice[selected]);
+
+            Select_Die(selected);
+        }
     }
 }
